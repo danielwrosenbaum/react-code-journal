0b5ddcfb12bff2ef3b12e6a6957c44776996ebc6
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-05-03 21:09:38 +0000

    added hash routing and renderPage function to app.jsx. #2

--- client/app.jsx
+++ client/app.jsx
@@ -1,16 +1,41 @@
+
 import React from 'react';
-import Create from './pages/create'
+import parseRoute from '../../Final-Project/client/lib/parse-route';
+import PageContainer from './components/page-container';
+import Create from './pages/create';
+import Entries from './pages/entries';
 
 export default class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
+      route: parseRoute(window.location.hash)
 
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({ route: parseRoute(window.location.hash) });
+    });
+  }
+
+  renderPage() {
+    const { route } = this.state;
+    if (route.path === '' || route.path === 'create') {
+      return <Create />;
+    }
+    if (route.path === 'entries') {
+      return <Entries />;
     }
   }
+
   render() {
+    // const { route } = this.state;
     return (
-      <Create />
-    )
+      <PageContainer>
+        {this.renderPage()}
+      </PageContainer>
+    );
   }
 }
