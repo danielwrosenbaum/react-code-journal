629969f026762c57516dcf8d240bc015aa2581b9
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-05-03 20:52:17 +0000

    added get request to express server in index.js. #2

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv/config');
 const express = require('express');
 const db = require('./db');
-const staticMiddleware = require('./static-middleware')
+const staticMiddleware = require('./static-middleware');
 
 const app = express();
 const jsonMiddleware = express.json();
@@ -9,20 +9,33 @@ const jsonMiddleware = express.json();
 app.use(staticMiddleware);
 app.use(jsonMiddleware);
 
+app.get('/api/codeJournal', (req, res, next) => {
+  const sql = `
+  select *
+    from "journal"
+    order by "entryId" DESC
+  `;
+  db.query(sql)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => next(err));
+});
+
 app.post('/api/codeJournal', (req, res, next) => {
-  const {photoUrl, title, notes } = req.body;
+  const { photoUrl, title, notes } = req.body;
   const sql = `
   insert into "journal" ("title", "photoUrl", "notes")
   values ($1, $2, $3)
   returning *
-  `
+  `;
   const params = [title, photoUrl, notes];
   db.query(sql, params)
     .then(result => {
-      res.status(201).json(result.rows[0])
+      res.status(201).json(result.rows[0]);
     })
-    .catch(err => next(err))
-})
+    .catch(err => next(err));
+});
 app.listen(process.env.PORT, () => {
-  console.log(`express server listening on port ${process.env.PORT}`)
-})
+  console.log(`express server listening on port ${process.env.PORT}`);
+});
