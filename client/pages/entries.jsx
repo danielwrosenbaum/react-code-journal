c93e6264a521255ddf9a7cd5d64cd20572ed5df7
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-05-03 21:43:20 +0000

    added map function to render entries from database onto entries page. #2

--- client/pages/entries.jsx
+++ client/pages/entries.jsx
@@ -26,8 +26,46 @@ export default class Entries extends React.Component {
   }
 
   render() {
+    const { result } = this.state;
+    if (!result) return null;
+    const entries = result;
+    const entryResults = (
+      <div className="entries-container">
+        {(result.length === 0) &&
+          <h2>Nothing Here!</h2>}
+        {
+          entries.map((entry, index) => {
+            const title = entry.title;
+            const photoUrl = entry.photoUrl;
+            const notes = entry.notes;
+            const createdAt = entry.createdAt;
+            return (
+              <div key={index} className="entry-card">
+                <div className='row'>
+                  <div className="col-half pic-container">
+                    <img className="pic" src={photoUrl} alt={title} />
+                  </div>
+                  <div className="col-half info-container">
+                    <div className="row">
+                      <h2>{title}</h2>
+                    </div>
+                    <div className="row">
+                      <p>{notes}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+
+          })
+        }
+      </div>
+
+    );
     return (
-      <div className="entries-container"></div>
+      <div className="entry-page">
+        {entryResults}
+      </div>
     );
   }
 }
