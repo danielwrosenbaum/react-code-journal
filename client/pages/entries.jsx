5c7ac22bc5e52db6ea65fad2229f4a1d651aefda
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-05-03 20:58:07 +0000

    added fetch request in componentdidmount to entries page. #2

--- client/pages/entries.jsx
+++ client/pages/entries.jsx
@@ -4,8 +4,25 @@ export default class Entries extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      result: null,
+      isLoading: true
 
     };
+  }
+
+  componentDidMount() {
+    fetch('/api/codeJournal')
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          result,
+          isLoading: false
+        });
+        console.log(result);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 
   render() {
