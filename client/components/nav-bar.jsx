efe20a1e242e10f06f37a776c02fb6b5d2671af6
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-05-24 23:59:44 +0000

    made cursor change to pointer whenever hovered over anchor element. #24

--- client/components/nav-bar.jsx
+++ client/components/nav-bar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 export default function Navbar() {
-
   const [searchValue, setSearchValue] = useState('');
 
   function handleSubmit() {
@@ -14,29 +13,28 @@ export default function Navbar() {
   }
 
   return (
-      <nav className="navbar">
-        <div className="row">
-          <div className="nav-container col-full">
-            <div className="col-half">
-              <div className="navbar-title">
-                <div className="one">CodeJournal</div>
-              </div>
-            </div>
-            <div className="navbar-under col-third">
-              <a className="navbar-entries" href="#create">
-                <div className="two">New</div>
-              </a>
-              <form className="form-group fg--search">
-                <input className="search" type="text" value={searchValue} placeholder="Search Entries" required onChange={() => setSearchValue(event.target.value)}></input>
-                <button type="submit" onClick={() => handleSubmit()}><i className="fa fa-search"></i></button>
-              </form>
-              <a className="navbar-entries" href="#entries">
-                <div className="two">Entries</div>
-              </a>
+    <nav className="navbar">
+      <div className="row">
+        <div className="nav-container col-full">
+          <div className="col-half">
+            <div className="navbar-title">
+              <div className="one">CodeJournal</div>
             </div>
           </div>
-
+          <div className="navbar-under col-third">
+            <a className="navbar-entries" href="#create">
+              <div className="two">New</div>
+            </a>
+            <form className="form-group fg--search">
+              <input className="search" type="text" value={searchValue} placeholder="Search Entries" required onChange={() => setSearchValue(event.target.value)}></input>
+              <button type="submit" onClick={() => handleSubmit()}><i className="fa fa-search"></i></button>
+            </form>
+            <a className="navbar-entries" href="#entries">
+              <div className="two">Entries</div>
+            </a>
+          </div>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 }
