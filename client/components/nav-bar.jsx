13a988498b3d9c92d794df1978ed3d54756e6577
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-05-24 23:41:23 +0000

    refactored nav-bar to use react hooks. #24

--- client/components/nav-bar.jsx
+++ client/components/nav-bar.jsx
@@ -1,31 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default class Navbar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchValue: ''
-    };
-    this.handleSearch = this.handleSearch.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+export default function Navbar() {
 
-  handleSearch(event) {
-    this.setState({ searchValue: event.target.value });
-  }
+  const [searchValue, setSearchValue] = useState('');
 
-  handleSubmit() {
+  function handleSubmit() {
     event.preventDefault();
-    if (!this.state.searchValue) {
+    if (!searchValue) {
       return null;
     }
-    window.location.hash = 'results?search=' + this.state.searchValue;
-    this.setState({ searchValue: '' });
+    window.location.hash = 'results?search=' + searchValue;
+    return setSearchValue('');
 
   }
 
-  render() {
-    return (
+  return (
       <nav className="navbar">
         <div className="row">
           <div className="nav-container col-full">
@@ -39,8 +28,8 @@ export default class Navbar extends React.Component {
                 <div className="two">New</div>
               </a>
               <form className="form-group fg--search">
-                <input className="search" type="text" value={this.state.searchValue} placeholder="Search Entries" required onChange={this.handleSearch}></input>
-                <button type="submit" onClick={this.handleSubmit}><i className="fa fa-search"></i></button>
+                <input className="search" type="text" value={searchValue} placeholder="Search Entries" required onChange={() => setSearchValue(event.target.value)}></input>
+                <button type="submit" onClick={() => handleSubmit()}><i className="fa fa-search"></i></button>
               </form>
               <a className="navbar-entries" href="#entries">
                 <div className="two">Entries</div>
@@ -50,6 +39,5 @@ export default class Navbar extends React.Component {
 
         </div>
       </nav>
-    );
-  }
+  );
 }
